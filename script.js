0daf7bd312f7f05d2e490201284b1c0f7aa72c56
Aditya <2000.aadi@gmail.com>
2022-01-09 19:54:32 +0530

    5 round game with results in console

--- script.js
+++ script.js
@@ -1,37 +1,73 @@
 let computerChoice, playerChoice;
+let winSentence = "You Win! ", loseSentence = "You Lose! ";
+let res, score=0;
 
+
+const values = ['Rock', 'Paper', 'Scissors'];
+
+// marks computerChoice randomly from above values array.
 function computerPlay (){
-    return Math.floor(Math.random()*3+1);
+    computerChoice = values[Math.floor(Math.random() * values.length)]; 
 };
 
-// 1=rock, 2=paper, 3=scissor;
-let compNum = computerPlay();
-let playerNum = prompt("Enter between 1 to 3: ");
 
-function mapper (num) {
-    if (num == 1) return 'rock';
-    else if (num == 2) return 'paper';
-    else if (num ==3) return 'scissor';
-    else return 'invalid selection';
-}
+// round played between input value (playerSelection) & computer value (computerSelection)
+// result returned as string. 
+function playRound (playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+    let Player = player.charAt(0).toUpperCase() + player.slice(1);
+    let computer = computerSelection.toLowerCase();
+    let Computer = computer.charAt(0).toUpperCase() + computer.slice(1);
 
-computerChoice = mapper(compNum);
-playerChoice = mapper(playerNum);
+    let playerVal = mapper(Player), compVal = mapper(Computer);
+    res = result(playerVal, compVal);
 
-console.log("your input is " + playerChoice);
-console.log("Computer choice is " + computerChoice);
+    if (playerVal == 4) return "Please input a valid value. You entered " + Player + '!';
 
-function result (playerNum, compNum)  {
-    if (playerNum==compNum) return "Match tied";
-    else if (playerNum < compNum){
-        if (playerNum==1 && compNum==3) return "You Won";
-        else return "You lost";
+    if (res == -1){
+        return loseSentence + Computer + ' beats ' + Player;
+    } else if (res == 1){
+        return winSentence + Player + ' beats ' + Computer;
     } else {
-        if (playerNum==3 && compNum==1) return "You lost";
-        else return "You won";
+        return 'Match Tied!';
     }
 }
 
-let matchResult = result (playerNum, compNum);
+// maps value of rock, paper and scissors to 1, 2 and 3 respectively
+function mapper (value) {
+    if (value == 'Rock') return 1;
+    else if (value == 'Paper') return 2;
+    else if (value == 'Scissors') return 3;
+    else return 4;
+}
 
-console.log(matchResult);
+// returns the result of a match. 0 = player lost, 1 = match tied, 2 = player won
+function result (playerNum, compNum)  {
+    if (playerNum==compNum) return 0;
+    else if (playerNum < compNum){
+        if (playerNum==1 && compNum==3) return 2;
+        else return -1;
+    } else {
+        if (playerNum==3 && compNum==1) return 0;
+        else return 1;
+    }
+}
+
+function game(){
+    score=0; res=0;
+    for (let i=0; i<=4; i++){
+        console.log("Round " + (i+1));
+
+        playerChoice = prompt("Enter your choice from Rock, Paper or Scissors: ");
+        computerPlay();
+
+        console.log("Your input is " + playerChoice);
+        console.log("Computer's choice is " + computerChoice);
+
+        console.log(playRound(playerChoice, computerChoice));
+        if (res==1) ++score;
+        console.log("Your score is " + score);
+    }
+}
+
+game();
